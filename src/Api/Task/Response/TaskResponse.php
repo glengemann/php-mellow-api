a248a9316610061ac6d710ea58be8eb4a896dedf
Guillermo Lengemann Garcés <guillermo.lengemann@gmail.com>
2026-04-26 15:10:34 -0500

    Update TaskResponse to include additional fields

--- src/Api/Task/Response/TaskResponse.php
+++ src/Api/Task/Response/TaskResponse.php
@@ -7,6 +7,20 @@ namespace Mellow\Api\Task\Response;
 class TaskResponse
 {
     public function __construct(
+        public readonly int $id,
+        public readonly string $uuid,
+        public readonly string $title,
+        public readonly string $description,
+        public readonly float $price,
+        /**
+         * @var array{
+         *     type: int,
+         *     triggerDate: string,
+         *     isComingUp: bool
+         * }
+         */
+        public readonly array $deadline,
+        public readonly array $worker,
         /** array{currency: Currency, id: int} */
         public readonly array $currency,
         public readonly int $state,
